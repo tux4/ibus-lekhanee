4b975e65a560227c034fdf899f633419595b5b49
Prasanna Suman <prasanna.tux@gmail.com>
2013-08-24 00:28:22 -0400

    More personalisation

--- engine/main.py
+++ engine/main.py
@@ -31,7 +31,9 @@ from engine import EngineEnchant
 
 class IMApp:
     def __init__(self, exec_by_ibus):
-        engine_name = "IBus Lekhanee" if exec_by_ibus else "IBus Lekhanee (dbg)"
+        engine_name = "IBus Lekhanee (dbg)"
+        
+        """
         self.__component = \
                 IBus.Component.new("org.freedesktop.IBus.Lekhanee",
                                    "IBus Lekhanee",
@@ -41,27 +43,27 @@ class IMApp:
                                    "http://github.com/tux4/ibus-lekhanee",
                                    "/usr/bin/exec",
                                    "ibus-lekhanee")
+
         engine = IBus.EngineDesc.new("IBus-Lekhanee",
                                      engine_name,
                                      "IBus Input Method for Indic Languages",
                                      "Indic",
                                      "GPL",
                                      "Prasanna Suman <prasanna.tux@gmail.com>",
-                                     "icons/ibus-lekhanee.svg",
+                                     "ibus-lekhanee.svg",
                                      "np")
         self.__component.add_engine(engine)
+        """
+        self.__component = IBus.Component.new_from_file("lekhanee.xml") 
         self.__mainloop = GLib.MainLoop()
         self.__bus = IBus.Bus()
         self.__bus.connect("disconnected", self.__bus_disconnected_cb)
         self.__factory = IBus.Factory.new(self.__bus.get_connection())
-        self.__factory.add_engine("IBus-Lekhanee",
-                GObject.type_from_name("IBus-Lekhanee"))
-        if exec_by_ibus:
-            self.__bus.request_name("org.freedesktop.IBus.Lekhanee", 0)
-        else:
-            self.__bus.register_component(self.__component)
-            self.__bus.set_global_engine_async(
-                    "IBus-Lekhanee", -1, None, None, None)
+        self.__factory.add_engine("Lekhanee",
+                GObject.type_from_name("Lekhanee"))
+        self.__bus.register_component(self.__component)
+        self.__bus.set_global_engine_async(
+            "Lekhanee", -1, None, None, None)
 
     def run(self):
         self.__mainloop.run()
@@ -74,45 +76,5 @@ def launch_engine(exec_by_ibus):
     IBus.init()
     IMApp(exec_by_ibus).run()
 
-def print_help(out, v = 0):
-    print >> out, "-i, --ibus             executed by IBus."
-    print >> out, "-h, --help             show this message."
-    print >> out, "-d, --daemonize        daemonize ibus"
-    sys.exit(v)
-
-def main():
-    try:
-        locale.setlocale(locale.LC_ALL, "")
-    except:
-        pass
-
-    exec_by_ibus = False
-    daemonize = False
-
-    shortopt = "ihd"
-    longopt = ["ibus", "help", "daemonize"]
-
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], shortopt, longopt)
-    except getopt.GetoptError, err:
-        print_help(sys.stderr, 1)
-
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print_help(sys.stdout)
-        elif o in ("-d", "--daemonize"):
-            daemonize = True
-        elif o in ("-i", "--ibus"):
-            exec_by_ibus = True
-        else:
-            print >> sys.stderr, "Unknown argument: %s" % o
-            print_help(sys.stderr, 1)
-
-    if daemonize:
-        if os.fork():
-            sys.exit()
-
-    launch_engine(exec_by_ibus)
-
 if __name__ == "__main__":
-    main()
+    launch_engine(False)
